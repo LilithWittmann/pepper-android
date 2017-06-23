5547c98d4eb8ab4660335abfb4f65b37d60d5208
Lilith Wittmann <mail@lilithwittmann.de>
2017-06-23 15:56:41 +0200

    feat(apis) adde basic speech recognition

---
+++ src/main/java/de/lilithwittmann/pepperandroid/api/ALSpeechRecognition.java
@@ -0,0 +1,36 @@
+package de.lilithwittmann.pepperandroid.api;
+
+import com.aldebaran.qi.Future;
+import com.aldebaran.qi.FutureFunction;
+
+import java.util.List;
+
+import de.lilithwittmann.pepperandroid.PepperAPI;
+import de.lilithwittmann.pepperandroid.PepperSession;
+
+/**
+ * http://doc.aldebaran.com/2-5/naoqi/audio/altexttospeech.html
+ */
+
+public class ALSpeechRecognition extends PepperAPI{
+
+    public ALSpeechRecognition(PepperSession session) throws Exception {
+        super(session, "ALSpeechRecognition");
+    }
+
+    public Future<Object> setVocabulary(List<String> vocabulary, Boolean enableWordSpotting) {
+        return this.API.call("setVocabulary", vocabulary, enableWordSpotting );
+    }
+
+    public Future<Object> getAvailableLanguages() {
+        return this.API.call("getAvailableLanguages");
+    }
+
+    public Future<Object> setLanguage(String language) {
+        return this.API.call("setLanguage", language);
+    }
+
+    public Future<Object> getLanguage() {
+        return this.API.call("getLanguage");
+    }
+}
